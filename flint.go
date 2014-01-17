b9fcdf493ec97085c9ab85e01d26d3d5f48d4824
Wynn Netherland <wynn@github.com>
2014-01-16 18:04:09 -0600

    Linter object

--- flint.go
+++ flint.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
 	"os"
+	"path/filepath"
 )
+
+type lint struct {
+	Path   string
+	Errors []string
+	status int
+}
+
+func (l *lint) findFile(pattern string) bool {
+	matches, _ := filepath.Glob(l.Path + "/" + pattern)
+	return len(matches) > 0
+}
+
+func (l *lint) CheckReadme() {
+	if !l.findFile("README*") {
+		l.Errors = append(l.Errors, "[ERROR] README not found")
+	}
+}
+
+func (l *lint) CheckContributing() {
+	if !l.findFile("CONTRIBUTING*") {
+		l.Errors = append(l.Errors, "[ERROR] CONTRIBUTING not found")
+	}
+}
+
+func (l *lint) CheckLicense() {
+	if !l.findFile("LICENSE*") {
+		l.Errors = append(l.Errors, "[ERROR] LICENSE not found")
+	}
+}
+
+func (l *lint) CheckBootstrap() {
+	if !l.findFile("script/bootstrap") {
+		l.Errors = append(l.Errors, "[ERROR] Bootstrap script not found")
+	}
+}
+
+func (l *lint) CheckTest() {
+	if !l.findFile("script/test") {
+		l.Errors = append(l.Errors, "[ERROR] Test script not found")
+	}
+}
 
 func main() {
 	app := cli.NewApp()
@@ -21,19 +64,31 @@ func main() {
 		if len(c.Args()) > 0 {
 			path = c.Args()[0]
 		}
-		println(path)
+		linter := &lint{Path: path}
 
 		if !c.Bool("skip-readme") {
-			checkReadme(path)
+			linter.CheckReadme()
 		}
 		if !c.Bool("skip-contributing") {
-			checkContributing(path)
+			linter.CheckContributing()
+		}
+		if !c.Bool("skip-license") {
+			linter.CheckLicense()
+		}
+		if !c.Bool("skip-bootstrap") {
+			linter.CheckBootstrap()
+		}
+		if !c.Bool("skip-test") {
+			linter.CheckTest()
+		}
+
+		if len(linter.Errors) > 0 {
+			for _, element := range linter.Errors {
+				fmt.Println(element)
+			}
+			os.Exit(1)
 		}
 	}
 
 	app.Run(os.Args)
-}
-
-func checkReadme(path string) {
-	println("Checking", path)
 }
---
+++ flint_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/bmizerany/assert"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	assert.Equal(t, 1 == 1, true)
+}
